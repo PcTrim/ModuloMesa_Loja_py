0b2e6b8347ecd7cea5b7ed4b66de9859834b7335
PcTrim <comdado_inf@hotmail.com>
2026-06-30 15:38:13 -0300

    Update version.py

--- PcTrim Commerce/version.py
+++ PcTrim Commerce/version.py
@@ -3,7 +3,7 @@
 import os
 import re
 
-APP_VERSION = "265-3"
+APP_VERSION = "265-4"
 
 _VERSION_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "version.py")
 _VERSION_RE = re.compile(r"""^APP_VERSION\s*=\s*["']([^"']+)["']""", re.M)
